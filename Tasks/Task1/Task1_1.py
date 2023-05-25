9b68ddaa0a476b8fcefe702420ea7f8c6422d2a0
MikitaMakarau <375298330439@mail.ru>
2023-05-25 21:33:44 +0300

    add Task

---
+++ Tasks/Task1/Task1_1.py
@@ -0,0 +1,6 @@
+#запрос
+x = float(input("Enter temperature in degrees Celsius\n"))
+#вычисление
+y = 1.8 * x + 32
+#вывод результата
+print(y, "degrees fahrenheit")
---
+++ Tasks/Task1/Task1_3.py
@@ -0,0 +1,6 @@
+#запрос
+x = float(input("Enter km/h\n"))
+#вычисление
+y = x * 1000 / 3600
+#вывод результата
+print("it's meters per second",(round(y,2)))
