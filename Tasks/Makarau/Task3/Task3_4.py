113ed29110d790f4e65814371f9716b7e63c120a
MikitaMakarau <375298330439@mail.ru>
2023-06-15 13:53:45 +0300

    Update Task3_4.py

--- Tasks/Makarau/Task3/Task3_4.py
+++ Tasks/Makarau/Task3/Task3_4.py
@@ -1,12 +1,11 @@
 list = input("Enter a list of numbers separated by spaces: ").split()
-num_list = [int(i) for i in list]
-first_largest = num_list[0] 
-second_largest = num_list[0] 
-for i in range(1,len(num_list)): 
-    if num_list[i] > first_largest: 
-        first_largest = num_list[i] 
-for i in range(1,len(num_list)): 
-    if num_list[i] > second_largest and num_list[i] != first_largest: 
-        second_largest = num_list[i] 
+first_largest = list[0] 
+second_largest = list[0] 
+for i in range(1,len(list)): 
+    if list[i] > first_largest: 
+        first_largest = list[i] 
+    for i in range(1,len(list)): 
+        if list[i] > second_largest and list[i] != first_largest: 
+            second_largest = list[i] 
 print(second_largest)
     
