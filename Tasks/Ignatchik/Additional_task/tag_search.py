3b9e11522a296eb904eec73ea200d45c1d310f35
Andrey <student.andrey.2022@gmail.com>
2023-06-15 10:46:17 +0300

    added body and div

--- Tasks/Ignatchik/Additional_task/tag_search.py
+++ Tasks/Ignatchik/Additional_task/tag_search.py
@@ -1,45 +1,37 @@
-import re
-# Пока только закрывает <p>. Направление выбрано правельно?
 with open('tag_search.html', 'r', encoding='utf-8') as file:
     list_html = file.readlines()
 
-flag = True
-html_line = ""
-list_of_all_tags = []
-pattern = '<(\S*?)[^>]*>.*?|<.*?/>'
-
-with open("tag_search.html", "r") as input_file:
-    for line in input_file:
-        html_line += line
-
-all_tags = re.finditer(pattern, html_line)
-
-try:
-    while flag:
-        item = next(all_tags)
-        list_of_all_tags.append(item.group())
-except StopIteration:
-    flag = False
-
-set_tag = set(list_of_all_tags)
+# single_HTML_tags = ['<!DOCTYPE html>', '<area>', '<base>', '<br>', '<col>', '<embed>', '<hr>', '<img>',
+#                     '<input>', '<keygen>', '<link>', '<meta>', '<param>', '<source>', '<track>', '<wbr>']
 
 new_list_html = []
-for i in set_tag :
-    if '/' in i:
-        continue
+
+for i in list_html:
+    if '<body>' in i: # or '<head>' in i or '<html>' in i
+        new_list_html.append(i)
+    elif '<div>' in i:
+        new_list_html.append([i])
+    elif '</div>' in i:
+        new_list_html[-1].append(i)
     else:
-        n1 = list_of_all_tags.count(i)
-        n2 = list_of_all_tags.count(i[0]+'/'+i[1:])
-        if n1 != n2:
-            for j in list_html:
-                if i in j and j[-2] != '>':
-                    new_list_html.append(j[:-1]+'<'+'/'+i[1:]+j[-1])
-                else:
-                    new_list_html.append(j)
+        new_list_html[-1].append(i)
+
+for j in new_list_html:
+    for k in j:
+        if '<div>' in k:
+            if j[-1] == '    </div>':
+                j[-1] = '    </div>\n'
+            elif j[-1] != '    </div>\n':
+                j.append('    </div>\n')
+        if '<p>' in k:
+            if k[-5:-1] != '</p>':
+                ind = j.index(k)
+                j[ind] = k[:-1]+'</p>'+k[-1]
+
+if new_list_html[-1] != '</body>':
+    new_list_html.append('</body>')
 
 with open('output.html', 'w', encoding='utf-8') as file:
     for i in new_list_html:
-        file.write(i)
-
-
-
+        for j in i:
+            file.write(j)
