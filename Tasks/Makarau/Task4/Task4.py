ed185a7dcbe0884a06aa99f0dff3a878bb1f1c9c
MikitaMakarau <375298330439@mail.ru>
2023-06-22 11:12:24 +0300

    update task4

--- Tasks/Makarau/Task4/Task4.py
+++ Tasks/Makarau/Task4/Task4.py
@@ -1,9 +1,11 @@
 width = int(input("Enter the maximum integer number of characters in the string, which must be greater than 35:"))
+
 with open("text.txt", "r") as f:
     words=f.read().split()
     line = ""
     index = 0
     new_line = []
+
 for i in words:
     index+=len(i)
     if index>=width:
@@ -13,12 +15,14 @@ for i in words:
         line += " "
         index +=1
     line +=i
+
 for j in line.split("\n"):
     spaces = width - len(j)
     while spaces > 0:
         j = j.replace(" ","  ",spaces)
         spaces = width - len(j)
-    new_line.append(j + "\n")    
+    new_line.append(j + "\n")  
+
 with open ("new_text.txt", "w") as f:
     for i in new_line:
         f.write(i)
