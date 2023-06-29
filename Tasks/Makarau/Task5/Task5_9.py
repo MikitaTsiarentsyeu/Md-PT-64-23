4c4940b8dc3cd9dff2a65d16665ed15bf5dec472
MikitaMakarau <375298330439@mail.ru>
2023-06-29 10:04:41 +0300

    Update Task5_9.py

--- Tasks/Makarau/Task5/Task5_9.py
+++ Tasks/Makarau/Task5/Task5_9.py
@@ -1,9 +1,9 @@
-def degree (a, b, x=0, counter=1):    
+def degree (a, b, x):
     if b==0:
-        return print(f"Result {x}")  
-    if b!=0:  
-        x=a**counter      
-        return degree (a, b-1, x, counter+1)    
+        return print(f"Result {x}") 
+    if b !=0 :
+        x *=a
+        return degree (a, b-1, x)        
 a = int(input("Enter number: "))
 b = int(input("Enter the value of the degree: "))
-degree(a, b)
+degree(a, b, x=1)
