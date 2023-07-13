7b0c5da694a844fcaa947f529166861cf27e8547
Andrey <student.andrey.2022@gmail.com>
2023-07-13 11:54:44 +0300

    updated Task_6

--- Tasks/Ignatchik/Task_6/Task_6_1.py
+++ Tasks/Ignatchik/Task_6/Task_6_1.py
@@ -1,20 +1,8 @@
-import time
+def division(a,b):
+    try:
+        return a/b
+    except ZeroDivisionError:
+        return "Cannot divide by zero"
 
-def log(func):
-    def wraper(*args):
-        start = time.time()
-        value = func(*args)
-        end = time.time()
-        res = start - end
-        with open('log.txt','w',encoding='utf-8') as f:
-            result = f'Execution time {res} seconds,\narguments {args},\nvalue of a function {value}' 
-            f.write(result)
-        return value
-    return wraper
-
-@log
-def suma(a,b):
-    result = a+b
-    return result
-
-suma(3,5)
+x, y = int(input()), int(input())
+print(division(x,y))
--- Tasks/Ignatchik/Task_6/Task_6_2.py
+++ Tasks/Ignatchik/Task_6/Task_6_2.py
@@ -1,22 +1,8 @@
-def cache(func):
-    square = {}
-    def wraper(*args):
-        for i in args:
-            if i in square:
-                result = square[i]
-            else:
-                result = func(*args)
-                square[i] = result
-        return result
-    return wraper
-
-@cache
-def sq(a):
-    return a**2
-
-print(sq(1))
-print(sq(2))
-print(sq(3))
-print(sq(4))
-print(sq(5))
-print(sq(2))
+def file_to_line(a):
+    try:
+        with open(a,'r',encoding='utf-8') as f:
+            return f.read()
+    except FileNotFoundError:
+        return "File not found"
+    
+print(file_to_line('log.txt'))
